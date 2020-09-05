4754b74cde003ec113a8ee9f8463912ba55de023
mint-web <rlfalsk0301@naver.com>
2020-09-05 17:44:35 +0900

    responsive

--- js/common.js
+++ js/common.js
@@ -1,59 +1,82 @@
 (function ($) {
-	//menu scroll
-	var secLocation = [];
-	secLocation[0] = $('#about').offset().top;
-	secLocation[1] = $('#porfolio').offset().top;
+	// mobile navi
+	hamburgerArea();
 
-	var menuNum = 0;
+	function hamburgerArea() {
+		$('.nav-btn').on('click', function () {
+			$(this).toggleClass('close-btn');
+			$('#nav').toggleClass('open');
+			$('.dim').fadeToggle();
+		});
 
-	$('.navigation > li a').on({
-		click: function () {
-			menuNum = $(this).attr('class').slice(4, 5);
-			$('html, body').stop().animate({
-				scrollTop: secLocation[menuNum]
-			}, 500);
-		}
-	});
+		$('body').append('<div class="dim"></div>');
 
-	//Career tab
-	$('.about-tab li').click(function () {
-		var tabId = $(this).attr('data-tab');
+		$('.dim').on('click', function () {
+			$('.nav-btn').removeClass('close-btn');
+			$('#nav').removeClass('open');
+			$(this).fadeOut();
+		});
 
-		$('.about-tab li').removeClass('active');
-		$('.about-con').removeClass('active');
+		//menu scroll
+		var secLocation = [];
+		secLocation[0] = $('#about').offset().top;
+		secLocation[1] = $('#porfolio').offset().top;
 
-		$(this).addClass('active');
-		$('#' + tabId).addClass('active');
-	});
+		var menuNum = 0;
 
-
-	//Portfolio
-	$('.portfolio-list').owlCarousel({
-		loop: true,
-		nav: false,
-		autoplay: false,
-		autoplayHoverPause: false,
-		autoplayTimeout: 2000,
-		autoplaySpeed: 1000,
-		responsiveClass: true,
-		responsive: {
-			0: {
-				items: 1
-			},
-			680: {
-				items: 2
-			},
-			980: {
-				items: 3
+		$('.navigation > li>a').on({
+			click: function (e) {
+				e.preventDefault();
+				menuNum = $(this).attr('class').slice(4, 5);
+				$('html, body').stop().animate({
+					scrollTop: secLocation[menuNum]
+				}, 500);
 			}
-		}
-	});
-	//Portfolio detail
-	$('.more-btn').click(function () {
-		num = $(this).attr('class').slice(13, 15);
-		$('#page' + num).fadeIn(300);
-	});
-	$('.detail-bg, .pop-close').click(function () {
-		$('.detail-wrap').fadeOut(300);
-	})
+		});
+
+		//Career tab
+		$('.about-tab li').click(function () {
+			var tabId = $(this).attr('data-tab');
+
+			$('.about-tab li').removeClass('active');
+			$('.about-con').removeClass('active');
+
+			$(this).addClass('active');
+			$('#' + tabId).addClass('active');
+		});
+
+
+		//Portfolio
+		$('.portfolio-list').owlCarousel({
+			loop: true,
+			nav: false,
+			autoplay: false,
+			autoplayHoverPause: false,
+			autoplayTimeout: 2000,
+			autoplaySpeed: 1000,
+			responsiveClass: true,
+			responsive: {
+				0: {
+					items: 1
+				},
+				680: {
+					items: 2
+				},
+				980: {
+					items: 3
+				}
+			}
+		});
+		//Portfolio detail
+		$('.more-btn').click(function () {
+			num = $(this).attr('class').slice(13, 15);
+			$('#page' + num).fadeIn(300);
+		});
+		$('.detail-bg, .pop-close').click(function () {
+			$('.detail-wrap').fadeOut(300);
+		});
+
+
+	}
+
 })(jQuery);
